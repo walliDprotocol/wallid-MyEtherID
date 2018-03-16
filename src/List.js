2c4c65b0f90f547bbacba3e51a51d27ed12b9ed1
Adriano Campos <adrianoribeirocampos@gmail.com>
2018-03-16 14:37:21 +0000

    Change DemoBank to CrediBank

--- src/List.js
+++ src/List.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 // products will be presented by react-bootstrap-table
 var products = [{
-  Service: "DemoBank",
+  Service: "CrediBank",
   Entity: "Portuguese Republic",
   Card: "Cartão de Cidadão",
   Attributes: "Identity, Address",
-  SCaddress: "9h8gj79x",
+  SCaddress: "9h8gj79x9h8gj79x",
   TransactionDate: " 21 Jan 18"
 },
 {
@@ -14,7 +14,7 @@ var products = [{
   Entity: "Ord. Advg.",
   Card: "Ballot",
   Attributes: "Identity",
-  SCaddress: "8htr1d99",
+  SCaddress: "8htr1d998htr1d99",
   TransactionDate: "16 Fev 18"
 }
 ];
@@ -38,10 +38,10 @@ class List extends Component {
           >
           <TableHeaderColumn dataField="Service" width='20%'>Service</TableHeaderColumn>
           <TableHeaderColumn dataField="Entity" width='20%' isKey={true}>Entity</TableHeaderColumn>
-          <TableHeaderColumn dataField="Card" width='20%'>Card</TableHeaderColumn>
-          <TableHeaderColumn dataField="Attributes" width='20%'>Attributes</TableHeaderColumn>
-          <TableHeaderColumn dataField="SCaddress" width='10%'>SC Address</TableHeaderColumn>
-          <TableHeaderColumn dataField="TransactionDate" width='10%'>Transaction Date</TableHeaderColumn>
+          <TableHeaderColumn dataField="Card" width='15%'>Card</TableHeaderColumn>
+          <TableHeaderColumn dataField="Attributes" width='15%'>Attributes</TableHeaderColumn>
+          <TableHeaderColumn dataField="SCaddress" width='15%'>SC Address</TableHeaderColumn>
+          <TableHeaderColumn dataField="TransactionDate" width='15%'>Transaction Date</TableHeaderColumn>
         </BootstrapTable>
       </div>
     );
