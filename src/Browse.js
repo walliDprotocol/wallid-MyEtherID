18ab22291362b90ab49bdfd7a72416b2d015e485
Adriano Campos <adrianoribeirocampos@gmail.com>
2018-03-23 12:44:40 +0000

    Show info in browse page read from blockchain

--- src/Browse.js
+++ src/Browse.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import Web3 from 'web3'
+import BlockIdContract from './blockid/BlockId.js'
 
-// products will be presented by react-bootstrap-table
+window.addEventListener('reload', function () {
+
+  if(typeof web3 !== 'undefined'){
+    console.log("Using web3 detected from external source like Metamask")
+    window.web3 = new Web3(window.web3.currentProvider)
+  }else{
+    console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+    alert('No web3? You should consider trying MetaMask!')
+  }
+
+});
+
 var products = [
 {
   Entity: "Portuguese Republic",
@@ -10,18 +24,22 @@ var products = [
   Attributes: "Identity, Address",
   SCaddress: "9h8gj79x9h8gj79x",
   SCDate: " 12 Jan 18"
-},
-{
-  Entity: "Ord. Advogados",
-  IDCertificate: "x 509",
-  Card: "Ballot",
-  Attributes: "Identity",
-  SCaddress: "8htr1d998htr1d99",
-  SCDate: "08 Fev 18"
 }
 ];
 
 class Browse extends Component {
+
+  isExpandableRow(row) {
+    return true;
+  }
+
+  expandComponent(row) {
+
+    return (
+      <BSTable data={ row.expand } />
+    );
+  }
+
   render() {
     return (
       <div>
@@ -37,7 +55,8 @@ class Browse extends Component {
           hover
           condensed
           pagination
-          search
+          expandableRow={ this.isExpandableRow }
+          expandComponent={ this.expandComponent }
           >
           <TableHeaderColumn dataField="Entity" width='20%' isKey={true}>Entity</TableHeaderColumn>
           <TableHeaderColumn dataField="IDCertificate" width='10%'>ID Certificate</TableHeaderColumn>
@@ -52,3 +71,75 @@ class Browse extends Component {
 }
 
 export default Browse;
+
+class BSTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+      ContractAddress : '0x82209352470b2f22f5a6874790114d5651a75285',
+      ContractInstance : null
+    };
+    if(window.web3){
+      const MyContract = window.web3.eth.contract(BlockIdContract.abi)
+      this.state.ContractInstance = MyContract.at(this.state.ContractAddress)
+
+      this.state.ContractInstance.countItemList( (err, data) => {
+        console.log('Count items :  ', data);
+        console.log('total items #', data.c[0] );
+      });
+
+      this.getInfo();
+    }
+  }
+
+  hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 2; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+  }
+
+  getInfo()
+  {
+    this.state.ContractInstance.getInfo( (err, data) => {
+      console.log('get Info Result ', data);
+      var loadData = [];
+      var dataAttribute = JSON.parse(this.hex2a(data[1]))
+      //console.log(dataAttribute);
+      //console.log(Object.keys(dataAttribute));
+      for(var i in dataAttribute){
+        //console.log(i);
+        //console.log(dataAttribute[i]);
+        loadData.push({ 'item' : i, 'value' : dataAttribute[i]})
+      }
+
+      dataAttribute = JSON.parse(this.hex2a(data[2]))
+      //console.log(dataAttribute);
+      //console.log(Object.keys(dataAttribute));
+      for(i in dataAttribute){
+        //console.log(i);
+        //console.log(dataAttribute[i]);
+        loadData.push({ 'item' : i, 'value' : dataAttribute[i]})
+      }
+      this.state.data = loadData
+      //console.log(this.state.data);
+    });
+  }
+  render() {
+    if (this.state.data) {
+      return (
+        <BootstrapTable
+          data={this.state.data}
+          hover
+          condensed
+          >
+          <TableHeaderColumn dataField="item" width='20%' isKey={true}>Item</TableHeaderColumn>
+          <TableHeaderColumn dataField="value" width='10%'>Value</TableHeaderColumn>
+        </BootstrapTable>);
+      } else {
+        return (<p>There is no data to display</p>);
+      }
+    }
+  }
--- src/components/ImportForm.jsx
+++ src/components/ImportForm.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import $ from 'jquery'; 
 import Web3 from 'web3'
 import BlockIdContract from '../blockid/BlockId.js'
 
-
 window.addEventListener('reload', function () {
-  
-  if(typeof web3 != 'undefined'){
-      console.log("Using web3 detected from external source like Metamask")
-      window.web3 = new Web3(window.web3.currentProvider)
+  if(typeof web3 !== 'undefined'){
+    console.log("Using web3 detected from external source like Metamask")
+    window.web3 = new Web3(window.web3.currentProvider)
   }else{
-      console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-      window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+    console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
   }
-
 });
-
-
 
 class ImportForm extends React.Component {
   constructor(props) {
@@ -28,37 +22,27 @@ class ImportForm extends React.Component {
       ContractAddress : '0x82209352470b2f22f5a6874790114d5651a75285',
       ContractInstance : null
     };
-    
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    const MyContract = window.web3.eth.contract(BlockIdContract.abi)
-    this.state.ContractInstance = MyContract.at(this.state.ContractAddress)
+    if(window.web3){
+      const MyContract = window.web3.eth.contract(BlockIdContract.abi)
+      this.state.ContractInstance = MyContract.at(this.state.ContractAddress)
 
-    this.state.ContractInstance.countItemList( (err, data) => {
-      console.log('Count items :  ', data);
-      console.log('total items #', data.c[0] );
-    });
-
-    //this.getInfo();
-
+      this.state.ContractInstance.countItemList( (err, data) => {
+        console.log('Count items :  ', data);
+        console.log('total items #', data.c[0] );
+      });
+    }
   }
 
   hex2a(hexx) {
     var hex = hexx.toString();//force conversion
     var str = '';
     for (var i = 2; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
-  }
-
-  getInfo()
-  {
-    this.state.ContractInstance.getInfo( (err, data) => {
-      console.log('get Info Result ', data);
-        console.log('id Attr ', this.hex2a(data[1]) );
-        console.log('user address ', this.hex2a(data[2]));
-    });
   }
 
   handleChange(event) {
@@ -68,7 +52,7 @@ class ImportForm extends React.Component {
   }
 
   handleSubmit(event) {
-    
+
     console.log('WalletAddress :' + this.state.walletAddress);
     console.log('Password : ' + this.state.password);
     console.log('Data :' + this.state.data);
@@ -78,25 +62,21 @@ class ImportForm extends React.Component {
     var idAttr = JSON.stringify( obj.id_attributes);
     var address = JSON.stringify( obj.address_attributes);
     console.log('user address ',obj.address_attributes );
-    
+
     this.state.ContractInstance.addInfo( idAttr ,  address , (err, data) => {
-        console.log('add info result is ', data);
+      console.log('add info result is ', data);
     });
-    
-    //BlockIdLib.addInfo( this.state.walletAddress, '', '');
+
     event.preventDefault();
   }
 
   /* run after component render */
   componentDidMount(){
-
   }
 
   /* run before component render */
-  componentWillMount(){   
-  
+  componentWillMount(){
   }
-
 
   render() {
     return (
@@ -114,27 +94,27 @@ class ImportForm extends React.Component {
         {/*<div class="form-group">
           <label>BlockID Encryption Password</label>
           <input type="password" name="password" onChange={this.handleChange} class="form-control" placeholder="Create the password to encrypt and then later decrypt your certified ID attributes" required />
-        </div>
-        <p><a href="https://blockid.herokuapp.com">What is BlockID Encrytion Password?</a></p>
-        <div class="form-group">
+          </div>
+          <p><a href="https://blockid.herokuapp.com">What is BlockID Encrytion Password?</a></p>
+          <div class="form-group">
           <label>Confirm BlockID Encryption Password:</label>
           <input type="password" name="password" onChange={this.handleChange} class="form-control" placeholder="Create the password to encrypt and later decrypt your certified ID attributes" required />
-        </div>
-        */}
-        <div class="form-group">
-          <label>ID Data:</label>
-          <textarea id="importData" name="data" onChange={this.handleChange} class="form-control" rows="5" placeholder="Paste your ID Data provided by BlockID’s Import ID App" required></textarea>
-        </div>
-        <input type="submit" value="Connect to metamask" /> Recommended action
-        <p><a href="https://metamask.io/">what is Metamask?</a></p>
-          <div class="form-group">
-            <label>Paste Private Key: Not recommended</label>
-            <input type="password" name="password" onChange={this.handleChange} class="form-control" placeholder="Paste Private Key: Not recommended" />
           </div>
-          <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+          */}
+          <div class="form-group">
+            <label>ID Data:</label>
+            <textarea id="importData" name="data" onChange={this.handleChange} class="form-control" rows="5" placeholder="Paste your ID Data provided by BlockID’s Import ID App" required></textarea>
+          </div>
+          <input type="submit" value="Connect to metamask" /> Recommended action
+            <p><a href="https://metamask.io/">what is Metamask?</a></p>
+            <div class="form-group">
+              <label>Paste Private Key: Not recommended</label>
+              <input type="password" name="password" onChange={this.handleChange} class="form-control" placeholder="Paste Private Key: Not recommended" />
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
+    }
 
-export default ImportForm;
+    export default ImportForm;
